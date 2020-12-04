ebb85e85eb2bfc8117d40559f88a24bde4566640
Stephanie Marsh <adamsstephaniem@gmail.com>
2020-12-04 10:38:03 -0500

    refactor into class, make method reusable

--- Year_2020/day_01/day_01.py
+++ Year_2020/day_01/day_01.py
@@ -1,24 +1,29 @@
 from my_utils import parseInputsToInt
 
-TARGET_SUM = 2020
 
-def find_pair_with_sum_2020(input):
-	input = parseInputsToInt('Year_2020/day_01/input.txt') if not input else input
-	candidates = _build_dict(input)
-	for number in input:
-		counterpart = TARGET_SUM - number
-		if candidates.get(counterpart):
-			print('pair:', number, counterpart)
-			return number * counterpart
-	raise Exception('No values found for target sum')
-	
+class Day01():
+	def __init__(self, input=None):
+		self.input = parseInputsToInt('Year_2020/day_01/input.txt') if input is None else input
+		self.target_grand_total = 2020
 
-def _build_dict(input):
-	candidates = {}
-	for number in input:
-		candidates[number] = True
-	return candidates
+	def find_pair_with_sum(self, target_sum):
+		candidates = self._build_dict()
+		for number in self.input:
+			counterpart = target_sum - number
+			if candidates.get(counterpart):
+				print('pair:', number, counterpart)
+				return number * counterpart
+		raise Exception('No values found for target sum')
+
+	def find_three_with_sum(self, target_sum):
+		pass
+
+	def _build_dict(self):
+		candidates = {}
+		for number in self.input:
+			candidates[number] = True
+		return candidates
 
 
 if __name__ == '__main__':
-	print(find_pair_with_sum_2020(None))
+	print(Day01().find_pair_with_sum(2020))
--- Year_2020/day_01/test_day_01.py
+++ Year_2020/day_01/test_day_01.py
@@ -1,13 +1,20 @@
 import unittest
 from unittest.mock import patch
-from day_01 import find_pair_with_sum_2020
+from day_01 import Day01
 
-class Day01Part1(unittest.TestCase):
+class TestDay01(unittest.TestCase):
 
-	def test_example_input(self):
+	def test_example_input_part_1(self):
 		input = [1721, 979, 366, 299, 675, 456]	
 		expected = 514579
-		result = find_pair_with_sum_2020(input)
+		result = Day01(input).find_pair_with_sum(2020)
+		print('result: ', result)
+		assert expected == result
+
+	def test_example_input_part_2(self):
+		input = [1721, 979, 366, 299, 675, 456]	
+		expected = 241861950
+		result = Day01(input).find_three_with_sum(input)
 		print('result: ', result)
 		assert expected == result
 
