42b14d89f836b08e0f4b378b8c90ac35450e27f7
Stephanie Marsh <adamsstephaniem@gmail.com>
2020-12-04 11:36:23 -0500

    working solution to D01P02

--- Year_2020/day_01/day_01.py
+++ Year_2020/day_01/day_01.py
@@ -2,21 +2,29 @@ from my_utils import parseInputsToInt
 
 
 class Day01():
-	def __init__(self, input=None):
-		self.input = parseInputsToInt('Year_2020/day_01/input.txt') if input is None else input
+	def __init__(self, input=[]):
+		self.input = (
+			parseInputsToInt('Year_2020/day_01/input.txt') if not input else input
+		)
+		self.input.sort()
 		self.target_grand_total = 2020
 
-	def find_pair_with_sum(self, target_sum):
+	def find_pair_with_sum(self, target_sum, skip_number=0):
 		candidates = self._build_dict()
 		for number in self.input:
 			counterpart = target_sum - number
-			if candidates.get(counterpart):
-				print('pair:', number, counterpart)
+			if candidates.get(counterpart) and candidates[counterpart] != skip_number:
+				print('matches:', number, counterpart, skip_number or '')
 				return number * counterpart
-		raise Exception('No values found for target sum')
+			if number >= target_sum:
+				return 0
 
-	def find_three_with_sum(self, target_sum):
-		pass
+	def find_three_with_sum(self):
+		for number in self.input:
+			target_sum = self.target_grand_total - number
+			match = self.find_pair_with_sum(target_sum, number)
+			if match:
+				return match * number
 
 	def _build_dict(self):
 		candidates = {}
@@ -27,3 +35,4 @@ class Day01():
 
 if __name__ == '__main__':
 	print(Day01().find_pair_with_sum(2020))
+	print(Day01().find_three_with_sum())
--- Year_2020/day_01/test_day_01.py
+++ Year_2020/day_01/test_day_01.py
@@ -7,14 +7,14 @@ class TestDay01(unittest.TestCase):
 	def test_example_input_part_1(self):
 		input = [1721, 979, 366, 299, 675, 456]	
 		expected = 514579
-		result = Day01(input).find_pair_with_sum(2020)
+		result = Day01(input=input).find_pair_with_sum(2020)
 		print('result: ', result)
 		assert expected == result
 
 	def test_example_input_part_2(self):
 		input = [1721, 979, 366, 299, 675, 456]	
 		expected = 241861950
-		result = Day01(input).find_three_with_sum(input)
+		result = Day01(input=input).find_three_with_sum()
 		print('result: ', result)
 		assert expected == result
 
