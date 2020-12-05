d4fab9783fe2d316a1d2c86f796d846ce60bdb2d
Stephanie Marsh <adamsstephaniem@gmail.com>
2020-12-05 16:08:43 -0500

    comments and cleanup

--- Year_2020/day_01/day_01.py
+++ Year_2020/day_01/day_01.py
@@ -1,22 +1,28 @@
-from my_utils import parseInputsToInt
+from .my_utils import parseInputsToInt
 
 
 class Day01():
+	"""
+		Assumptions:
+		 - input values are between 1 and target - 1
+		 - input has no duplicate values
+	"""
 	def __init__(self, input=[]):
 		self.input = (
 			parseInputsToInt('Year_2020/day_01/input.txt') if not input else input
 		)
 		self.input.sort()
-		self.target_grand_total = 2020
 
 	def find_pair_with_sum(self, target_sum, skip_number=0):
 		candidates = self._build_dict()
 		for number in self.input:
 			counterpart = target_sum - number
 			if candidates.get(counterpart) and candidates[counterpart] != skip_number:
+				# matching value exists and is not a duplicate of the first number
 				print('matches:', number, counterpart, skip_number or '')
 				return number * counterpart
 			if number >= target_sum:
+				# stop searching when remaining values in sorted list are too large
 				return 0
 
 	def find_three_with_sum(self, target_sum):
--- Year_2020/day_01/test_day_01.py
+++ Year_2020/day_01/test_day_01.py
@@ -1,5 +1,4 @@
 import unittest
-from unittest.mock import patch
 from day_01 import Day01
 
 class TestDay01(unittest.TestCase):
