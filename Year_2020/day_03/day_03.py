4698ecc92290723b856ae94e7fd93e1c728dd735
Stephanie Marsh <adamsstephaniem@gmail.com>
2020-12-06 22:24:10 -0500

    refactor Part 1 method to handle other inputs

--- Year_2020/day_03/day_03.py
+++ Year_2020/day_03/day_03.py
@@ -6,14 +6,15 @@ class Day03:
 		self.input = self._read_input_file() if not input else input
 		self.hill = self._parse_data(self.input)
 
-	def count_trees_3_by_1(self):
-		index = 0
-		x_increment = 3
+	def count_trees_by_slope(self, x_increment=3, y_increment=1):
+		x_index = 0
 		count_trees = 0
-		for row in self.hill:
-			if row[index] == TREE:
-				count_trees += 1
-			index = (index + x_increment) % len(row)
+		for y_index, row in enumerate(self.hill):
+			if y_index % y_increment == 0:
+				if row[x_index] == TREE:
+					count_trees += 1
+				x_index = (x_index + x_increment) % len(row)
+			
 		return count_trees
 
 	def _read_input_file(self):
@@ -26,4 +27,11 @@ class Day03:
 		return data.split('\n')
 
 if __name__ == '__main__':
-	print(Day03().count_trees_3_by_1())
+	slope_1_1 = Day03().count_trees_by_slope(x_increment=1)
+	slope_3_1 = Day03().count_trees_by_slope()
+	slope_5_1 = Day03().count_trees_by_slope(x_increment=5)
+	slope_7_1 = Day03().count_trees_by_slope(x_increment=7)
+	slope_1_2 = Day03().count_trees_by_slope(x_increment=1, y_increment=2)
+	print('Part 1:', slope_3_1)
+	print(f'Part 2: {slope_1_1}*{slope_3_1}*{slope_5_1}*{slope_7_1}*{slope_1_2} =', \
+		slope_1_1 * slope_3_1 * slope_5_1 * slope_7_1 * slope_1_2)
--- Year_2020/day_03/test_day_03.py
+++ Year_2020/day_03/test_day_03.py
@@ -7,8 +7,34 @@ INPUT = '..#.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.
 class TestDay03(unittest.TestCase):
 
 	def test_example_input_part_1(self):
-		result = Day03(input=INPUT).count_trees_3_by_1()
+		result = Day03(input=INPUT).count_trees_by_slope()
 		expected = 7
+		assert result == expected
+
+	def test_example_input_part_2_1_by_1(self):
+		result = Day03(input=INPUT).count_trees_by_slope(x_increment=1)
+		expected = 2
+		assert result == expected
+
+	def test_example_input_part_2_5_by_1(self):
+		result = Day03(input=INPUT).count_trees_by_slope(x_increment=5)
+		expected = 3
+		assert result == expected
+
+	def test_example_input_part_2_7_by_1(self):
+		result = Day03(input=INPUT).count_trees_by_slope(x_increment=7)
+		expected = 4
+		assert result == expected
+
+	def test_example_input_part_2_1_by_2(self):
+		result = Day03(input=INPUT).count_trees_by_slope(x_increment=1, y_increment=2)
+		expected = 2
+		assert result == expected
+
+	# adding a test case where y_increment changes the outcome
+	def test_example_input_part_2_3_by_2(self):
+		result = Day03(input=INPUT).count_trees_by_slope(x_increment=3, y_increment=2)
+		expected = 2
 		assert result == expected
 
 if __name__ == '__main__':
