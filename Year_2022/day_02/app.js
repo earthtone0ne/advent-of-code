91301faf9419ce4f3a67ca35e8773f537aa246bf
Stephanie Marsh <adamsstephaniem@gmail.com>
2022-12-02 20:28:33 -0500

    day 2 part 2

--- Year_2022/day_02/app.js
+++ Year_2022/day_02/app.js
@@ -1,41 +1,43 @@
 const input = require('./input.js');
 // Rock > Scissors, Scissors > Paper, Paper > Rock
 // them: A: rock, B: paper, C: scissors
-// you : X: rock, Y: paper, Z: scissors
+// x: lose, y: draw, z: win
 // win: 6, draw: 3, loss: 0
 // points: rock: 1, paper: 2, scissors: 3
 
 // split input into rounds, each round into a tuple
 const rounds = input.split('\n').map(r => r.split(' '));
 const [A,B,C,X,Y,Z] = "ABCXYZ";
+const RESULTS = { win: Z, draw: Y, loss: X, };
 const player1 = { rock: A, paper: B, scissors: C, };
 const player2 = { rock: X, paper: Y, scissors: Z, };
-const scoreByOutcome = { win: 6, draw: 3, loss: 0, };
+const scoreByOutcome = { Z: 6, Y: 3, X: 0, };
 const scoreByType = { [player2.rock]: 1, [player2.paper]: 2, [player2.scissors]: 3, };
 
-const outcomes = {
+const plays = {
 	[player1.rock]: {
-		[player2.paper]: scoreByOutcome.win,
-		[player2.scissors]: scoreByOutcome.loss,
-		[player2.rock]: scoreByOutcome.draw,
+		[RESULTS.draw]: player2.rock,
+		[RESULTS.loss]: player2.scissors,
+		[RESULTS.win]: player2.paper,
 	},
 	[player1.paper]: {
-		[player2.scissors]: scoreByOutcome.win,
-		[player2.rock]: scoreByOutcome.loss,
-		[player2.paper]: scoreByOutcome.draw,
+		[RESULTS.draw]: player2.paper,
+		[RESULTS.loss]: player2.rock,
+		[RESULTS.win]: player2.scissors,
 	},
 	[player1.scissors]: {
-		[player2.rock]: scoreByOutcome.win,
-		[player2.paper]: scoreByOutcome.loss,
-		[player2.scissors]: scoreByOutcome.draw,
+		[RESULTS.draw]: player2.scissors,
+		[RESULTS.loss]: player2.paper,
+		[RESULTS.win]: player2.rock,
 	},
 }
 
 const score = rounds.reduce( (acc, curr) => {
-	const [theirs, mine] = curr;
-	const outcome = outcomes[theirs][mine];
-	const typeScore = scoreByType[mine];
-	return acc + outcome + typeScore;
+	const [theirPlay, outcome] = curr;
+	const myPlay = plays[theirPlay][outcome];
+	const typeScore = scoreByType[myPlay];
+	const outcomeScore = scoreByOutcome[outcome];
+	return acc + outcomeScore + typeScore;
 }, 0);
 
 console.log(score);
