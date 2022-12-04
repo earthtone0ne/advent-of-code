10babb2218447cf2524fca81fb6ccace14ff5d3a
Stephanie Marsh <adamsstephaniem@gmail.com>
2022-12-04 12:09:03 -0500

    test run

---
+++ Year_2022/day_03/app.js
@@ -0,0 +1,45 @@
+// Rucksack reorg
+// split array into 2 halves
+// find common letter
+// count points of letter:
+// [a-z]: 1-26 (ascii: 097-122, diff 96)
+// [A-Z]: 27-52 (ascii: 065-090, diff 38)
+
+const testData = `vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`;
+
+function getCompartments(string) {
+	const mid = string.length / 2;
+	return [string.slice(0, mid), string.slice(mid)];
+}
+
+function findCommonChar([first, second]) {
+	return first.split('').find(char => second.includes(char));
+}
+
+function findPoints(char) {
+	// TODO: replace magic numbers with consts
+	const code = char.charCodeAt(0);
+	if (code >= 65 && code <= 90) {
+		return code - 38;
+	} else if (code >= 97 && code <= 122) {
+		return code - 96;
+	}
+}
+
+function go(data) {
+	const sacks = data.split('\n');
+	return sacks.reduce((acc, sack) => {
+		const compartments = getCompartments(sack);
+		const char = findCommonChar(compartments);
+		return acc += findPoints(char);
+	}, 0);
+}
+
+const expected = 157;
+const actual = go(testData);
+console.assert(expected === actual, actual);
