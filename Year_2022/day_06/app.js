5d65835cb317ef9f0c1deca4a00aaeba7694c8a5
Stephanie Marsh <adamsstephaniem@gmail.com>
2022-12-06 20:16:13 -0500

    Day 6 part 2

--- Year_2022/day_06/app.js
+++ Year_2022/day_06/app.js
@@ -20,18 +20,28 @@ function findStartSequence(input, size = 4) {
 	return result;
 }
 
-console.log(findStartSequence(input));
+console.log("part 1: ", findStartSequence(input));
+console.log("part 2: ", findStartSequence(input, 14));
 
-const testInputs = [
+
+// tests
+const testInputs1 = [
 	'bvwbjplbgvbhsrlpgdmjqwftvncz', // 5
 	'nppdvjthqldpwncqszvftbrmjlhg', // 6
 	'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', // 10
 	'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', // 11
-]
+];
 const expected1 = [5, 6, 10, 11];
-const actual1 = testInputs.map(s => findStartSequence(s));
-testInputs.forEach((str, idx) => console.assert(expected1[idx] === actual1[idx], actual1[idx]))
+const actual1 = testInputs1.map(s => findStartSequence(s));
+testInputs1.forEach((str, idx) => console.assert(expected1[idx] === actual1[idx], actual1[idx]));
 
+const testInputs2 = [
+	'mjqjpqmgbljsphdztnvjfqwrcgsmlb', // 19
+	'bvwbjplbgvbhsrlpgdmjqwftvncz', // 23
+	'nppdvjthqldpwncqszvftbrmjlhg', // 23
+	'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', // 29
+	'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', // 26
+];
 const expected2 = [19, 23, 23, 29, 26];
-const actual2 = testInputs.map(s => findStartSequence(s, 14));
-testInputs.forEach((str, idx) => console.assert(expected2[idx] === actual2[idx], actual2[idx]))
+const actual2 = testInputs2.map(s => findStartSequence(s, 14));
+testInputs2.forEach((str, idx) => console.assert(expected2[idx] === actual2[idx], actual2[idx]));
