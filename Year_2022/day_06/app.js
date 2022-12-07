fef6794df6252aa6aa3e1ebf5d0a9e4b5699805b
Stephanie Marsh <adamsstephaniem@gmail.com>
2022-12-06 20:12:00 -0500

    day 6 part 1

---
+++ Year_2022/day_06/app.js
@@ -0,0 +1,37 @@
+// Day 6: Tuning Trouble
+/*
+look for string of 4 different chars
+report index + 1 of 4th char
+*/
+const input = require('./input');
+
+function findStartSequence(input, size = 4) {
+	const str = input.split('');
+	let cursor = size;
+	let result;
+
+	while (!result && cursor <= str.length) {
+		const set = new Set(str.slice(cursor - size, cursor));
+		if (set.size === size) {
+			result = cursor;
+		}
+		cursor++;
+	}
+	return result;
+}
+
+console.log(findStartSequence(input));
+
+const testInputs = [
+	'bvwbjplbgvbhsrlpgdmjqwftvncz', // 5
+	'nppdvjthqldpwncqszvftbrmjlhg', // 6
+	'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', // 10
+	'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', // 11
+]
+const expected1 = [5, 6, 10, 11];
+const actual1 = testInputs.map(s => findStartSequence(s));
+testInputs.forEach((str, idx) => console.assert(expected1[idx] === actual1[idx], actual1[idx]))
+
+const expected2 = [19, 23, 23, 29, 26];
+const actual2 = testInputs.map(s => findStartSequence(s, 14));
+testInputs.forEach((str, idx) => console.assert(expected2[idx] === actual2[idx], actual2[idx]))
